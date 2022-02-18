c3b42b252238a87ba56aa92b5f7cac882fc64b42
Jeremy Osztreicher <jeremy.osztreicher@movember.com>
2022-02-18 19:14:23 +1100

    Initiate environment

---
+++ src/components/RobotController/RobotController.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Container } from "./RobotController.style";
+
+const RobotController = () => {
+    return(
+        <Container>
+            Hello World
+        </Container>
+    )
+}
+export default RobotController;
---
+++ src/components/RobotController/RobotController.style.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+    display: block;
+    width: 400px;
+    height: 600px;
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    top: 0;
+    margin: auto;
+    background-color: #fff;
+    color: #000;
+`
+export {Container}
