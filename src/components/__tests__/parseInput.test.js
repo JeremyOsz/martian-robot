f4b621f369413de3c79006dbcc9af03baffd267e
Jeremy Osztreicher <jeremy.osztreicher@movember.com>
2022-02-18 20:48:13 +1100

    Setup basic tests for inputs

--- src/components/__tests__/parseInput.test.js
+++ src/components/__tests__/parseInput.test.js
@@ -1,6 +1,51 @@
-import parseInput from "../../helpers/parseInput/parseInput";
+import {parseInput, validateInstructions, validatePosition} from "../../helpers/parseInput/parseInput";
+
+// Plain text inputs
+const testInput1 = `
+    53
+    11E 
+    RFRFRFRF
+`
+const testInput2 = `
+    32N 
+    FRRFLLFFRRFLL
+`
+const testInput3 = `
+    03W 
+    LLFFFLFLFL
+`
+
+//Splitting inputs into strings for simplicity
+
+const testBoundry = '53'
+const robotPosition1 = '11E'
+const robotInstructions1 = 'RFRFRFRF'
+
+const robotPosition2 = '32N'
+const robotInstructions2 = 'FRRFLLFFRRFLL'
+
+const robotPosition3 = '03W'
+const robotInstructions3 = 'LLFFFLFLFL'
+
+
+console.log("test Input=" + testInput1[0])
 
 test('parseInput will reject a non-string', () => {
-    expect(()=>{parseInput(1)}).toThrow('Input is not a String')
+    expect(()=>{parseInput(1)}).toThrow()
 })
+
+test('valid input will return true', () => {
+    expect(parseInput(robotPosition1, robotInstructions1)).toBe(true)
+})
+
+test('valid position will return true', () => {
+    expect(validatePosition(robotPosition1)).toBe(true)
+})
+
+test('valid instructions will return true', () => {
+    expect(validateInstructions(robotInstructions1)).toBe(true)
+})
+
+
+
 
--- src/helpers/parseInput/parseInput.js
+++ src/helpers/parseInput/parseInput.js
@@ -1,8 +1,23 @@
-const parseInput = (inputString) => {
-    if(typeof input !== 'string'){
-        throw Error('Input is not a String')
+const parseInput = (position, instructions) => {
+    //Parse if inputs are valid
+    if(validatePosition(position) && validateInstructions(instructions)){
+        return true
     }
-    return true
+
 }
 
-export default parseInput;
+const validatePosition = (position) =>{
+    if(typeof position !== 'string' || position.length !== 3){
+        throw Error('Invalid Position')
+    }
+    return true;
+}
+
+const validateInstructions = (instructions) =>{
+    if(typeof instructions !== 'string' || instructions.length < 1){
+        throw Error('Invalid Position')
+    }
+    return true;
+}
+
+export {parseInput, validatePosition, validateInstructions};
